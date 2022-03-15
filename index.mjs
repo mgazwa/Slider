7d6972faa2393e38dd4494bfcb9127ba104ca97a
mgazwa <mgazwa@gmail.com>
2022-03-15 22:23:58 +0100

    first styles added

--- index.mjs
+++ index.mjs
@@ -1,6 +1,10 @@
 import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
 
 const firstSlider = {
+    navigation: {
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+    }
 
 }
 
